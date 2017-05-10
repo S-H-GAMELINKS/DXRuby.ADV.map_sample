2acfecf006ca70c9e0a0dea792623a702913a7e9
S.H <gamelinks007@gmail.com>
2017-05-10 21:23:07 +0900

    マウスの位置によって地名を変更して描画するように変更

--- mapsystem_sample.rb
+++ mapsystem_sample.rb
@@ -12,18 +12,62 @@ Window.caption = ("次回作用マップシステム案")
 map = Image.new(640, 480, [0, 0, 0, 0])
 map = Image.load("map.png")
 
+#フォントサイズ
+map_font = Font.new(32)
+font = Font.new(26)
+
+#マップ名変数
+map_name = "出雲の国"
+ap_name = ""
+
 #マップ描画ループ
 Window.loop do
 
-#マウスの位置を取得
-x = Input.mouse_x
-y = Input.mouse_y
+	#マウスの位置を取得
+	x = Input.mouse_x
+	y = Input.mouse_y
 
 	#マップの描画
 	Window.draw(0, 0, map, 1)
 
-	#地名(アクセスポイント)の□描画
-	
+	#地名(アクセスポイント)の◆描画
+	Window.draw_font(100, 200, "◆", map_font, z:2)
+	Window.draw_font(50, 100, "◆", map_font, z:2)
+	Window.draw_font(150, 240, "◆", map_font, z:2)
+	Window.draw_font(200, 300, "◆", map_font, z:2)
+	Window.draw_font(350, 300, "◆", map_font, z:2)
+	Window.draw_font(250, 220, "◆", map_font, z:2)
+
+	#地名（アクセスポイント）の描画
+	Window.draw_font(500, 200, ap_name, font, z:2)
+
+	#マップ名描画
+	Window.draw_font(500, 450, map_name, font, z:2)
+
+	#マウスの位置によりマップ名を変更
+	if  x <= 100 && y <= 200 then
+		ap_name = "稲佐の浜"
+	end
+
+	if  x <= 50 && y <= 100 then
+		ap_name = "出雲大社"
+	end
+
+	if  x <= 150 && y <= 240 then
+		ap_name = "日御碕灯台"
+	end
+
+	if  x <= 200 && y <= 300 then
+		ap_name = "須佐神社"
+	end
+
+	if  x <= 350 && y <= 300 then
+		ap_name = "立久恵峡"
+	end
+
+	if  x <= 250 && y <= 220 then
+		ap_name = "旧大社駅"
+	end
 
 	#エスケープキーでループを抜ける
 	if Input.key_push?(K_ESCAPE) then
