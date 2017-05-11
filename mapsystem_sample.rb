85557a0414594ef3248f828a73b073642bbf2154
S.H <gamelinks007@gmail.com>
2017-05-11 12:35:41 +0900

    マウス位置での地名変更を少し手直し

--- mapsystem_sample.rb
+++ mapsystem_sample.rb
@@ -32,9 +32,9 @@ Window.loop do
 
 	#地名(アクセスポイント)の◆描画
 	Window.draw_font(100, 200, "◆", map_font, z:2)
-	Window.draw_font(50, 100, "◆", map_font, z:2)
-	Window.draw_font(150, 240, "◆", map_font, z:2)
-	Window.draw_font(200, 300, "◆", map_font, z:2)
+	Window.draw_font(150, 100, "◆", map_font, z:2)
+	Window.draw_font(180, 240, "◆", map_font, z:2)
+	Window.draw_font(240, 340, "◆", map_font, z:2)
 	Window.draw_font(350, 300, "◆", map_font, z:2)
 	Window.draw_font(250, 220, "◆", map_font, z:2)
 
@@ -45,27 +45,27 @@ Window.loop do
 	Window.draw_font(500, 450, map_name, font, z:2)
 
 	#マウスの位置によりマップ名を変更
-	if  x <= 100 && y <= 200 then
+	if x >= 70 && x <= 100 &&  y >= 170 && y <= 200 then
 		ap_name = "稲佐の浜"
 	end
 
-	if  x <= 50 && y <= 100 then
+	if  x >= 120 && x <= 150 &&  y >= 70 && y <= 100 then
 		ap_name = "出雲大社"
 	end
 
-	if  x <= 150 && y <= 240 then
+	if  x >= 150 && x <= 180 && y >= 210 && y <= 240 then
 		ap_name = "日御碕灯台"
 	end
 
-	if  x <= 200 && y <= 300 then
+	if  x >= 210 && x <= 240 && y >= 310 && y <= 340 then
 		ap_name = "須佐神社"
 	end
 
-	if  x <= 350 && y <= 300 then
+	if  x >= 320 && x <= 350 && y >= 270 && y <= 300 then
 		ap_name = "立久恵峡"
 	end
 
-	if  x <= 250 && y <= 220 then
+	if  x >= 220 && x <= 250 && y >= 190 && y <= 220 then
 		ap_name = "旧大社駅"
 	end
 
